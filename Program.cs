2e2647c54343439d66958bf344738a2959519179
Aaron Schlorke <aschlorke@yahoo.com>
2020-12-17 17:04:24 -0500

    INitial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3D_FlightSim
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
